0e0dfb8382007d31a84efdf964ba3f6b174164c5
margaret1123 <margaretzhao2610@gmail.com>
2026-07-16 20:34:45 +1200

    refactor: remove runtime image hydrator from English homepage

--- app/en/page.tsx
+++ app/en/page.tsx
@@ -1,11 +1,5 @@
-import { PromoImageHydrator } from "../promo-image-hydrator";
 import { SciNestHome } from "../scinest-home";
 
 export default function EnglishHomePage() {
-  return (
-    <>
-      <PromoImageHydrator locale="en" />
-      <SciNestHome locale="en" />
-    </>
-  );
+  return <SciNestHome locale="en" />;
 }
